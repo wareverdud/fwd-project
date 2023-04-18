cad708b3df5442adc8af020d6ce5dade9f784201
Nikita Nekhay <ktofreesapiens@gmail.com>
2023-04-18 09:41:58 +0300

    I have sorted and moved components for Layout, Home in appropriate? files; Add A little bit of style on language page

--- src/pages/languages/index.tsx
+++ src/pages/languages/index.tsx
@@ -1,8 +1,16 @@
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+import { SelectedPage } from '@/shared/types'
 
 export default function Languages(props: { user: { uid: any } }) {
+
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Languages
+  )
+
+
   const router = useRouter()
   function handle(lang: string) {
     router.push(`/languages/${lang}`).then((r) => r)
@@ -19,30 +27,30 @@ export default function Languages(props: { user: { uid: any } }) {
       <Head>
         <title>Languages</title>
       </Head>
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Languages)}>
       
-      
-      <h1 className="text-3xl font-bold">Languages</h1>
-      <div className="m-3">
-        <button onClick={() => handle('french')} className={buttonStyles}>
-          French
-        </button>
-      </div>
-      <div className="m-3">
-        <button onClick={() => handle('german')} className={buttonStyles}>
-          German
-        </button>
-      </div>
-      <div className="m-3">
-        <button onClick={() => handle('russian')} className={buttonStyles}>
-          Russian
-        </button>
-      </div>
-      <div className="m-3">
-        <button onClick={() => handle('spanish')} className={buttonStyles}>
-          Spanish
-        </button>
-      </div>
-      
+        <h1 className="text-3xl font-bold">Languages</h1>
+        <div className="m-3">
+          <button onClick={() => handle('french')} className={buttonStyles}>
+            French
+          </button>
+        </div>
+        <div className="m-3">
+          <button onClick={() => handle('german')} className={buttonStyles}>
+            German
+          </button>
+        </div>
+        <div className="m-3">
+          <button onClick={() => handle('russian')} className={buttonStyles}>
+            Russian
+          </button>
+        </div>
+        <div className="m-3">
+          <button onClick={() => handle('spanish')} className={buttonStyles}>
+            Spanish
+          </button>
+        </div>
+      </motion.div>
     </section>
   )
 }
