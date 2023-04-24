63ae3e38691ae8e47664a9c04567970a5a7e0d34
meldilen <123398352+meldilen@users.noreply.github.com>
2023-04-24 13:28:55 +0300

    Fixed

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -60,9 +60,6 @@ export default function AboutUs(props: { user: { uid: any } }) {
     )
   }
 
-  const buttonStyles =
-    'rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white'
-
   return (
     <section
       id="AboutUs"
