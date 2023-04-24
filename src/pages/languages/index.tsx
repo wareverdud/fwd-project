e69556c7c1478969c570ac01e7398584320d401c
meldilen <123398352+meldilen@users.noreply.github.com>
2023-04-24 19:52:28 +0300

    Languages page design

--- src/pages/languages/index.tsx
+++ src/pages/languages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { SelectedPage } from '@/shared/types'
+import pattern1 from '@/assets/Pattern4.png'
+import pattern2 from '@/assets/Arrow.png'
+import pattern3 from '@/assets/AbstractWaves.png'
 
 export default function Languages(props: { user: { uid: any } }) {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -13,9 +16,6 @@ export default function Languages(props: { user: { uid: any } }) {
   function handle(lang: string) {
     router.push(`/languages/${lang}`).then((r) => r)
   }
-
-  const buttonStyles =
-    'rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white'
 
   return (
     <section
@@ -28,26 +28,68 @@ export default function Languages(props: { user: { uid: any } }) {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Languages)}
       >
-        <h1 className="text-3xl font-bold">Languages</h1>
-        <div className="m-3">
-          <button onClick={() => handle('french')} className={buttonStyles}>
-            French
-          </button>
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -75 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <h1 className="ms-70 sticky left-80 right-0 top-0 text-center text-3xl font-bold underline">
+            Languages
+          </h1>
+          <p className="py-5 text-center text-base font-normal">
+            Choose the language you would like to learn:
+          </p>
+        </motion.div>
+        <button
+          onClick={() => handle('french')}
+          className="m-3 ml-[31vw] rounded-lg bg-red-400 px-11 py-3 text-white hover:bg-secondary-500"
+        >
+          French
+        </button>
+        <button
+          onClick={() => handle('spanish')}
+          className="ml-[1vw] rounded-lg bg-red-400 px-10 py-3 text-white hover:bg-secondary-500"
+        >
+          Spanish
+        </button>
+        <button
+          onClick={() => handle('german')}
+          className="m-3 ml-[31vw] rounded-lg bg-red-400 px-10 py-3 text-white hover:bg-secondary-500"
+        >
+          German
+        </button>
+        <button
+          onClick={() => handle('russian')}
+          className="ml-[1vw] rounded-lg bg-red-400 px-10 py-3 text-white hover:bg-secondary-500"
+        >
+          Russian
+        </button>
+        <div className="ml-[45vw] mt-[-12vw] flex">
+          <img
+            className="mx-auto w-[10vw]"
+            src={pattern2.src}
+            alt="Pattern_arrow"
+          />
         </div>
-        <div className="m-3">
-          <button onClick={() => handle('german')} className={buttonStyles}>
-            German
-          </button>
+        <div className="ml-[-45vw] mt-[-3vw]">
+          <img
+            className="mx-auto flex w-[12vw] py-10"
+            src={pattern1.src}
+            alt="Pattern_cube"
+          />
         </div>
-        <div className="m-3">
-          <button onClick={() => handle('russian')} className={buttonStyles}>
-            Russian
-          </button>
-        </div>
-        <div className="m-3">
-          <button onClick={() => handle('spanish')} className={buttonStyles}>
-            Spanish
-          </button>
+        <div className="ml-[55vw] mt-[-13vw]">
+          <img
+            className="mx-auto flex w-[12vw] py-10"
+            src={pattern3.src}
+            alt="Pattern_abstractWaves"
+          />
         </div>
       </motion.div>
     </section>
