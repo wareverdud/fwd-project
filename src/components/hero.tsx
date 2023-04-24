17bb7cfe0607102e0e59dc7e2aa3f426882da27e
meldilen <123398352+meldilen@users.noreply.github.com>
2023-04-24 12:57:21 +0300

    Made About us page and fixed some code

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,9 +3,6 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import ActionButton from '@/shared/ActionButton'
 import HomePageText from '@/assets/HomePageText.png'
 import HomePageGraphic from '@/assets/HomePageGraphic.png'
-import SponsorRedBull from '@/assets/SponsorRedBull.png'
-import SponsorForbes from '@/assets/SponsorForbes.png'
-import SponsorFortune from '@/assets/SponsorFortune.png'
 import { motion } from 'framer-motion'
 
 type Props = {
